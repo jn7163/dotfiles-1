a5169740b283b42a867d1736416d183d37970f8d
Havanna <skykiss_2000@163.com>
2012-05-31 20:54:01 +0800

    update some

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -17,18 +17,18 @@ altkey = "Mod1"
 
 layouts =
 {
-    awful.layout.suit.tile,             1
-    awful.layout.suit.tile.left,        2
-    awful.layout.suit.tile.bottom,      3
-    awful.layout.suit.tile.top,         4
-    awful.layout.suit.fair,             5
-    awful.layout.suit.fair.horizontal,  6
-    awful.layout.suit.spiral,           7
-    awful.layout.suit.spiral.dwindle,   8
-    awful.layout.suit.max,              9
-    awful.layout.suit.max.fullscreen,   10
-    awful.layout.suit.magnifier,        11
-    awful.layout.suit.floating          12
+    awful.layout.suit.tile,             --1
+    awful.layout.suit.tile.left,        --2
+    awful.layout.suit.tile.bottom,      --3
+    awful.layout.suit.tile.top,         --4
+    awful.layout.suit.fair,             --5
+    awful.layout.suit.fair.horizontal,  --6
+    awful.layout.suit.spiral,           --7
+    awful.layout.suit.spiral.dwindle,   --8
+    awful.layout.suit.max,              --9
+    awful.layout.suit.max.fullscreen,   --10
+    awful.layout.suit.magnifier,        --11
+    awful.layout.suit.floating          --12
 }
 
 tags = {
@@ -65,12 +65,12 @@ internet = {
     { "Openfetion", "openfetion" },
     { "Irssi", "xterm -e screen irssi" },
     { "Mutt", "xterm -e mutt" },
-    { "Gtmess", "xterm -e gtmess" },
+    { "Gtmess", "xterm -e gtmess" }
 }
 
 editors = {
     { "Leafpad", "leafpad" },
-    { "Emacs", "emacs" },
+    { "Emacs", "emacs" }
 }
 
 office = {
@@ -84,7 +84,7 @@ media = {
     { "Gimp", "gimp" },
     { "Cheese", "cheese" },
     { "Deadbeef", "deadbeef" },
-    { "Moc", "xterm -e mocp" },
+    { "Moc", "xterm -e mocp" }
 }
 
 games = {
