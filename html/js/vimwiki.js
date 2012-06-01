17f8ff16c921b475d8ef4d5d0f636bee73628675
Havanna <skykiss_2000@163.com>
2012-06-01 22:35:56 +0800

    add Google Analytics Tracking Code

--- html/js/vimwiki.js
+++ html/js/vimwiki.js
@@ -169,10 +169,20 @@ $(document).ready(function() {
 
 //Google 统计
 //var _gaq = _gaq || [];
-//_gaq.push(['_setAccount', 'UA-1080811-17']);
+//_gaq.push(['_setAccount', 'UA-28211867-1']);
+//_gaq.push(['_setDomainName', 'ihavanna.org']);
 //_gaq.push(['_trackPageview']);
-
 //$.getScript('http://www.google-analytics.com/ga.js');
 
 
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-28211867-1']);
+_gaq.push(['_setDomainName', 'ihavanna.org']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
